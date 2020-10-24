0ea8cb4d886d2682a15619c26a571e96854b3c57
beetosu <cpwalsh@buffalo.edu>
2020-10-24 19:52:05 -0400

    fixed canvas

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ const input = document.getElementById('videoPlayer');
 const canvas = document.getElementById('overlay')
 const canvasContext = canvas.getContext("2d");
 var video = document.querySelector("#videoPlayer");
-let displaySize = {
-	width: input.offsetWidth,
-	height: input.offsetHeight
-};
 setUpCamera();
 
 setInterval(() => {
@@ -27,18 +23,19 @@ function setUpCamera() {
 }
 
 function createBlocks() {
-  canvasContext.clearRect(0,0,displaySize.width,displaySize.height);
-  console.log(video)
+  canvasContext.clearRect(0,0,640,480);
   canvasContext.drawImage(video,0,0);
-  for (let i = 0; i < displaySize.height; i=i+16) {
-    for (let p = 0; p < displaySize.width; p=p+16) {
-      //canvasContext.drawImage(getBlock(p,i),p,i,16,16);
+  let colors = canvasContext.getImageData(0, 0, 640, 480);
+  for (let i = 0; i < 480; i=i+16) {
+    for (let p = 0; p < 640; p=p+16) {
+      canvasContext.drawImage(getBlock(p,i, colors),p,i,16,16);
     }
   }
 }
 
-function getBlock(x, y) {
+function getBlock(x, y, colors) {
   let image = new Image();
+  console.log(colors[1])
   image.src = "textures/brick.png"
   return image
 }
